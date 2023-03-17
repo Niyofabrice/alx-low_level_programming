b49e0184312136b4d6ddcea05d0b6bbf4e410d12
fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-03-17 07:07:32 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -a program that prints a line with printf function
+ * Return: 0(Success)
+ */
+int main(void)
+{
+prinf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
