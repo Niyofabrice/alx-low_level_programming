65a2f56ef9ca28b8a6805c6cafef9e61c4fc9592
fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-03-18 12:52:43 -0700

    program that print all the numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - program all the numbers of base 16 in lowercase
+ * Return: 0 (Success);
+ */
+int main(void)
+{
+int n;
+int m;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+for (m = 97; m <= 102; m++)
+{
+putchar(m);
+}
+return (0);
+}
