0a8512dadb2780465f10da075d4975904e28fecf
fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-03-18 11:05:04 -0700

    program to print the alphabets in lowercase letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the alphabets in lowercase letters
+ * Return: 0(Success);
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+return (0);
+}
