a0c933d105207be83afdfc81d78b2f55e6fefa76
fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-03-18 10:37:00 -0700

    program to print the last digit of a number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,15 +14,15 @@ n = rand() - RAND_MAX / 2;
 ld = n % 10;
 if (ld > 5)
 {
-printf("the last digit of %d is %d and is greater than 5", n, ld);
+printf("last digit of %d is %d and is greater than 5", n, ld);
 }
 else if (ld == 0)
 {
-printf("the last digit of %d is %d and is equal to 0", n, ld);
+printf("last digit of %d is %d and is equal to 0", n, ld);
 }
 else
 {
-printf("the last digit of %d is %d and is less than 6 and is not 0", n, ld);
+printf("last digit of %d is %d and is less than 6 and is not 0", n, ld);
 }
 return (0);
 }
