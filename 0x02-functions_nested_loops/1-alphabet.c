1ad6a21a5f017b1d1dd6893c0fbedf61d98859ae
fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-03-21 13:07:32 -0700

    program to print the alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that prints the alphabets
+ * Return: 0 (Success);
+ */
+int main(void)
+{
+print_alphabet();
+return(0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,3 +3,14 @@ char output()
 printf("_putchar\n");
 return (0);
 }
+
+
+void print_alphabet(void)
+{
+char ch;
+for(ch = 97; ch <= 122; ch++)
+{
+_putchar(ch);
+}
+return (0);
+}
