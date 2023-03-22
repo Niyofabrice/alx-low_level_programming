0b9e87f8cf867c5602b1528773a6753e3cd7710c
fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-03-22 13:36:06 -0700

    program that checks if a character is an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.h
@@ -0,0 +1,17 @@
+#include "main"
+/**
+ * _isalpha - functions that checks for alphabetic character
+ * @c: a variable that stores the character
+ * Return: 0 or 1 depending on the conditions
+ */
+int _isalpha(int c)
+{
+if ((c > 96 && c < 123) && (c > 64 && c < 91))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
