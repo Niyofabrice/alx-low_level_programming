e29300acba1c506cd0997a7bcf2cf7ecb1d6998e
fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-03-27 03:22:42 -0700

    function that updates a variables value using pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that updates the value of a variable using pointers
+ * @n: the variable to store the value
+ * Return: 0 (Success)
+ */
+void reset_to_98(int *n){
+*n = 98;
+return n;
+}
