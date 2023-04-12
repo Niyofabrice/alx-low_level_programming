d549de877b11cdceb0ece06fd15b6749912e9e5f
fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-04-12 08:08:03 -0700

    finshing all my tasks, i will show you how great i am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,19 +10,20 @@ int count_word(char *s)
 	int flag, c, w;
 	flag = 0;
 	w = 0;
+
 	for (c = 0; s[c] != '\0'; c++)
 	{
-        	if (s[c] == ' ')
-        	{
-            		flag = 0;
-        	}
-        	else if (flag == 0)
-        	{
-            		flag = 1;
-            		w++;
-        	}
-    	}
-    	return (w);		
+		if (s[c] == ' ')
+		{
+			flag = 0;
+		}
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+	return (w);		
 }
 /**
  * **strtow - function that splits a string into words
@@ -36,9 +37,9 @@ char **strtow(char *str)
 	int i, k = 0, len = 0, words, c = 0, start, end;
 
 	while (*(str + len))
-    	{
-        	len++;
-    	}
+	{
+		len++;
+	}
 	words = count_word(str);
 	if (words == 0)
 	{
