f3a1dd1f1fb7402563eec87a34eca54a9109167e
fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-04-12 08:19:22 -0700

    finshing all my tasks, i will show you how great i am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,9 +8,8 @@
 int count_word(char *s)
 {
 	int flag, c, w;
-	flag = 0;
+	flag = w = 0;
 
-	w = 0;
 	for (c = 0; s[c] != '\0'; c++)
 	{
 		if (s[c] == ' ')
@@ -58,9 +57,7 @@ char **strtow(char *str)
 			{
 				end = i, tmp = (char *) malloc(sizeof(char) * (c + 1));
 				if (tmp == NULL)
-
 					return (NULL);
-
 				while (start < end)
 				{
 					*tmp++ = str[start++];
@@ -68,10 +65,8 @@ char **strtow(char *str)
 				*tmp = '\0', matrix[k] = tmp - c, k++, c = 0;
 			}
 		}
-		else if (c++ == 0)
-		
+		else if (c++ == 0)	
 			start = i;
-		
 	}
 	matrix[k] = NULL;
 	return (matrix);
