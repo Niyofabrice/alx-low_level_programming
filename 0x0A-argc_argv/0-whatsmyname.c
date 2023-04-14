de1c79d077057422fe7d3edda8df627e21bd48c1
fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-04-14 07:17:54 -0700

    program that prints its name and a program that prints the argument passed into it

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,10 +4,10 @@
  * main - program that prints its name followed by a new line
  * @argc: it counts the number of arguments passed in the terminal
  * @argv: is a vector that strores the string value of arguments passed
- * Return: the name of the file
+ * Return: 0(Success)
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	print("%s\n", argv[0]);
+	print("%s\n", *argv);
 	return (0);
 }
