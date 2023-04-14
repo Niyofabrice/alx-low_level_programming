34fa1d8361b90244fa9c0551451c0b13439629b8
fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-04-14 08:27:20 -0700

    program that multiplies two integers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+/**
+ * _atoi - function that converts string into integer
+ * @s: variable to  store the string value
+ * Return: integere
+ */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = d = n = len = f = digit = 0;
+
+	while (s[len] != '\0')
+
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+
+				digit = -digit;
+
+			n = n * 10 + digit;
+
+			f = 1;
+
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+
+				break;
+
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+
+		return (0);
+
+	return (n);
+}
 /**
  * main - program that multiplies two integers
  * @argc: counts the arguments passed into it
@@ -14,7 +59,7 @@ int main(int argc, char *argv[])
 
 	if (argc > 1)
 	{
-		mult = atoi(argv[1]) * atoi(argv[2]);
+		mult = _atoi(argv[1]) * _atoi(argv[2]);
 		printf("%d\n", mult);
 	}
 	else
