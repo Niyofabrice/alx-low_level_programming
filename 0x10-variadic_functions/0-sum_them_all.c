aa3b4009b51756bd94dadfc0ae075bc3f394e2f7
fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-04-24 04:28:09 -0700

    function that returns the sum of its parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -22,7 +22,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		sum = sum + va_arg(args, int);
+		sum = sum + va_arg(args, unsigned int);
 	}
 	va_end(args);
 	return (sum);
