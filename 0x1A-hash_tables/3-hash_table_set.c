4a4114ba99cfcf5906c463f195aa7a138e50b657
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-12-01 07:12:27 -0800

    task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 	{
 		return (0);
- 	}
+	}
 
 	value_copy = strdup(value);
 	if (value_copy == NULL)
@@ -39,17 +39,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	node = malloc(sizeof(hash_node_t *));
 	if (node == NULL)
-	{
 		return (0);
-	}
 	node->key = key;
 	node->value = value_copy;
 	node->next = NULL;
-
 	while (ptr->next != NULL)
-	{
 		ptr = ptr->next;
-	}
 	ptr->next = node;
 	return (1);
 }
