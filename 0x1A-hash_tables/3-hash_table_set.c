e51527c929556be095356b8e8a51c263a771a60d
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-12-01 07:29:17 -0800

    task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,23 +9,18 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index, i;
-	hash_node_t *node;
+	hash_node_t *new;
 	char *value_copy;
+	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-	{
 		return (0);
-	}
 
 	value_copy = strdup(value);
 	if (value_copy == NULL)
-	{
 		return (0);
-	}
 
 	index = key_index((const unsigned char *)key, ht->size);
-
 	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
@@ -35,12 +30,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (1);
 		}
 	}
-	node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
 		return (0);
-	node->key = key;
-	node->value = value_copy;
-	node->next = ht->array[index];
-	ht->array[index] = node;
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
 	return (1);
 }
