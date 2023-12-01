64474f250b724f8a350dd8a459ce678d67c8744b
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-12-01 07:10:15 -0800

    task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,33 +11,45 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index, i;
 	hash_node_t *node;
+	char *value_copy;
 
-	if (key == NULL)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	{
+		return (0);
+ 	}
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
 	{
 		return (0);
 	}
 
-	index = hash_djb2(key);
+	index = key_index((const unsigned char *)key, ht->size);
 
-	if (ht[index]->key == NULL)
+	for (i = index; ht->array[i]; i++)
 	{
-		ht[index]->key = key;
-		ht[index]->value = value;
-	}
-	else
-	{
-		hash_node_t *ptr = ht[index];
-
-		node = malloc(sizeof(hash_node_t *));
-		node->key = key;
-		node->value = value;
-		node->next = NULL;
-
-		while (ptr->next != NULL)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			ptr = ptr->next;
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
 		}
-		ptr->next = node;
 	}
+	hash_node_t *ptr = ht->array[index];
+
+	node = malloc(sizeof(hash_node_t *));
+	if (node == NULL)
+	{
+		return (0);
+	}
+	node->key = key;
+	node->value = value_copy;
+	node->next = NULL;
+
+	while (ptr->next != NULL)
+	{
+		ptr = ptr->next;
+	}
+	ptr->next = node;
 	return (1);
 }
