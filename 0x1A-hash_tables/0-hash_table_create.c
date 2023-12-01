4114a8bc2a92b1a434f06f0694f483a8e21c6239
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-12-01 04:54:53 -0800

    task0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Function that creates a hash table
+ * @size: the size of the array
+ * Return: pointer to the newly created hash table or NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht = malloc(sizeof(hash_node_t) * size);
+	if (ht == NULL)
+	{
+		return(1);
+	}
+
+	return(ht);
+}
