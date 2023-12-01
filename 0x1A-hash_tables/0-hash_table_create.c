433b9012a46d7d6009c99cf822be0b749814495a
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2023-12-01 05:14:32 -0800

    task0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,11 +8,25 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht = malloc(sizeof(hash_node_t) * size);
+	hash_table_t *ht;
+	unsigned long int i;
+	
+	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 	{
 		return(NULL);
 	}
-	free(ht);
+
+	ht->size = size
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+		return(NULL);
+	}
+
+	for (i = 0; i < ht->array[i]; i++)
+	{
+		ht->array[i] = NULL;
+	}
 	return(ht);
 }
